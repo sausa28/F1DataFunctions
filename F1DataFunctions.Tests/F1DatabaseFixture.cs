ecf24be538d6f4a774e5983707dec9b0d47c8538
Sausa <sausa28@proton.me>
2025-03-02 12:56:32 +0000

    Pull database name into a constant

--- F1DataFunctions.Tests/F1DatabaseFixture.cs
+++ F1DataFunctions.Tests/F1DatabaseFixture.cs
@@ -10,13 +10,15 @@ namespace F1DataFunctions.Tests;
 
 public class F1DatabaseFixture : IAsyncLifetime
 {
+    private const string DatabaseName = "formula1db";
     private MsSqlContainer _msSqlContainer;
     
     public string ConnectionString { get; private set; }
     
     public async Task InitializeAsync()
     {
-        // Need to run this first: systemctl --user start podman.socket
+        // May need to run this first: systemctl --user start podman.socket
+        // Or maybe: systemctl --user restart podman.service podman.socket
         Environment.SetEnvironmentVariable("DOCKER_HOST", "unix:///run/user/1000/podman/podman.sock");
         Environment.SetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED", "true");
         _msSqlContainer = new MsSqlBuilder()
@@ -26,7 +28,7 @@ public class F1DatabaseFixture : IAsyncLifetime
         CreateEmptyDatabase();
         var sqlConnection = new SqlConnectionStringBuilder(_msSqlContainer.GetConnectionString())
         {
-            InitialCatalog = "formula1db"
+            InitialCatalog = DatabaseName
         };
         ConnectionString = sqlConnection.ConnectionString;
     }
@@ -36,9 +38,9 @@ public class F1DatabaseFixture : IAsyncLifetime
         using var connection = new SqlConnection(_msSqlContainer.GetConnectionString());
         
         connection.Open();
-        var command = new SqlCommand("DROP DATABASE IF EXISTS formula1db; CREATE DATABASE formula1db", connection);
+        var command = new SqlCommand($"DROP DATABASE IF EXISTS {DatabaseName}; CREATE DATABASE {DatabaseName}", connection);
         command.ExecuteNonQuery();
-        connection.ChangeDatabase("formula1db");
+        connection.ChangeDatabase(DatabaseName);
         command = new SqlCommand("CREATE SCHEMA staging;", connection);
         command.ExecuteNonQuery();
         var sqlFiles = Directory.GetFileSystemEntries("/home/sausa/repos/F1DataFunctions/formula1db", "*.sql", SearchOption.AllDirectories);
